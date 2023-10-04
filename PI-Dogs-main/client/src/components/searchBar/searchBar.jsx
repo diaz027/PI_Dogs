6a136de1f163b55a2c768198146d4c154841eb61
Diaz027 <dami27cito@gmail.com>
2023-10-04 16:26:29 -0300

    vamos los pibes

--- PI-Dogs-main/client/src/components/searchBar/searchBar.jsx
+++ PI-Dogs-main/client/src/components/searchBar/searchBar.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react';
 import { getDogsName } from '../../Redux/actions';
-import style from './searchBar.module.css'
 import { useDispatch } from 'react-redux';
+import style from './searchBar.module.css'
 
 const SearchBar = () => {
-    const [name, setName] = useState('')
-    const dispatch = useDispatch()
-    const handleOnClick = () =>{
-       dispatch(getDogsName(name))
-       setName('')
-    }
+    const [name, setName] = useState('');
+    const dispatch = useDispatch();
+
+    const handleOnClick = async () => {
+        try {
+            const response = await dispatch(getDogsName(name)); //hacemos a la funcion de mi actions y en que caso de que no hace ealgo con ese nombre tira el alert
+
+            if (response && response.payload.length === 0) {
+                alert(`No se encontró ningún perro con el nombre "${name}"`);
+            }
+            setName('');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
     const handleName = (event) => {
         setName(event.target.value)
     }
-   return (
-    <div>
-        <input className={style.label} type='text' onChange={handleName} value={name}/>
-        <button className={style.button} onClick={handleOnClick}>agregar</button>
-    </div>
-   )
+    return (
+        <div>
+            <input className={style.label} type='text' onChange={handleName} value={name} />
+            <button className={style.button} onClick={handleOnClick}>agregar</button>
+        </div>
+    )
 }
 export default SearchBar;
--- PI-Dogs-main/client/src/views/home/home.jsx
+++ PI-Dogs-main/client/src/views/home/home.jsx
@@ -1,50 +1,44 @@
 import { useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { getDogs,} from '../../Redux/actions';
-// import axios from 'axios'
 import Cards from '../../components/cards/cards';
-
-
+import style from './home.module.css'
 
 //paginado
-
 const DOGS_PER_PAGE = 8;
 
-function Home() {
-
-  // dentro de una funcion iria esto abajo
-  const newDogs = useSelector(state => state?.dogs);
-  const [dogs, setDogs] = useState([...newDogs].splice(0, DOGS_PER_PAGE))
-  const [currentPage, setCurrentPage] = useState(0);
+const Home = () => {
+  const newDogs = useSelector((state) => state?.dogs);
   const totalDogs = newDogs?.length;
-  const totalPage = Math.floor(totalDogs/DOGS_PER_PAGE)
+  const totalPage = Math.ceil(totalDogs/DOGS_PER_PAGE);
 
-  const nextHandler = () => {
-    const nextPage = currentPage + 1;
-    const primerDogs = nextPage * DOGS_PER_PAGE;
-    if(primerDogs === totalDogs) return;
-    setDogs([...newDogs].splice(primerDogs, DOGS_PER_PAGE))
-    setCurrentPage(nextPage);
-  }
-  const prevHandler = () => {
-    const prevPage = currentPage - 1;
-    console.log(prevPage);
-    const primerDogs = prevPage * DOGS_PER_PAGE;
-    console.log(primerDogs);
-    if(prevPage < 0) return;
-    setDogs([...newDogs].splice(primerDogs, DOGS_PER_PAGE))
-    setCurrentPage(prevPage);
-  }
- 
+  const [currentPage, setCurrentPage] = useState(0);
 
   const dispatch = useDispatch();
-    useEffect(()=>{
-      dispatch(getDogs())
-    },[dispatch]); 
+  useEffect(() => {
+    dispatch(getDogs());
+  }, [dispatch]);
+
+  const startDogs = currentPage * DOGS_PER_PAGE;
+  const endDogs = startDogs + DOGS_PER_PAGE;
+  const dogsToDisplay = newDogs?.slice(startDogs, endDogs)
+
+  const nextHandler = () => {
+    if(currentPage < totalPage - 1 ) {
+      setCurrentPage(currentPage + 1)
+    }
+  };
+
+  const prevHandler = () => {
+    if(currentPage > 0 ) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+ 
 
     return (
-      <div >
-        <Cards dogs={dogs}/>
+      <div className={style.home}>
+        <Cards dogs={dogsToDisplay}/>
         <div>
           <button onClick={prevHandler} disabled={currentPage === 0} >Prev</button>
           <span style={{color:'white'}}>pagina: {currentPage + 1} de {totalPage} </span>
