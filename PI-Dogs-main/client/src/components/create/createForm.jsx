460f0817c0c1e955f45cebd6210736e415394f21
Diaz027 <dami27cito@gmail.com>
2023-10-05 12:50:14 -0300

    validacion

--- PI-Dogs-main/client/src/components/create/createForm.jsx
+++ PI-Dogs-main/client/src/components/create/createForm.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { createAllDog } from "../../Redux/actions";
+import validacion from "./validacion";
 import style from './create.module.css'
 
 const  CrearForm= () => {
+  const [errors, setErrors] = useState({});
   const [formData, setFormData] = useState({
     name: '',
-    imagen: "",
+    // imagen: "",
     minHeight: "",
     maxHeight: "",
     minWeight: "",
@@ -19,7 +21,16 @@ const  CrearForm= () => {
       ...formData,
       [event.target.name]: event.target.value,
     });
+
+
+    setErrors(
+      validacion({
+        ...formData,
+        [event.target.name]: event.target.value,
+      })
+    );
   }
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,21 +43,27 @@ const  CrearForm= () => {
             <form onSubmit={handleSubmit}>
                 <label >Nombre</label>
                 <input type="text" name="name" value={formData.name} onChange={handleChange}/>
+                {errors.name && <p style = {{color: 'white', fontSize: '20px'}}>{errors.name}</p>}
 
                 <label >AlturaMin</label>
                 <input type="text" name="minHeight" value={formData.minHeight} onChange={handleChange}/>
+                {errors.minHeight && <p style = {{color: 'white', fontSize: '20px'}}>{errors.minHeight}</p>}
 
                 <label >AlturaMax</label>
                 <input  type="text" name="maxHeight" value={formData.maxHeight} onChange={handleChange}/>
+                {errors.maxHeight && <p style = {{color: 'white', fontSize: '20px'}}>{errors.maxHeight}</p>}
 
                 <label >PesoMin</label>
                 <input  type="text" name="minWeight" value={formData.minWeight} onChange={handleChange}/>
+                {errors.minWeight && <p style = {{color: 'white', fontSize: '20px'}}>{errors.minWeight}</p>}
 
                 <label >PesoMax</label>
                 <input  type="text" name="maxWeight" value={formData.maxWeight} onChange={handleChange}/>
+                {errors.maxWeight && <p style = {{color: 'white', fontSize: '20px'}}>{errors.maxWeight}</p>}
 
                 <label >años de vida</label>
                 <input  type="text" name="life_span" value={formData.life_span} onChange={handleChange}/>
+                {errors.life_span && <p style = {{color: 'white', fontSize: '20px'}}>{errors.life_span}</p>}
 
                 <button >Crear</button>
             </form>
--- PI-Dogs-main/client/src/components/create/validacion.jsx
+++ PI-Dogs-main/client/src/components/create/validacion.jsx
@@ -1,3 +1,25 @@
 const validacion =  (FormData) =>{
-    
-}
+    const errors = {};
+    if(!/^[A-Za-z]+$/i.test(FormData.name)){
+        errors.name='solo letras, no tiene que tener ningun numero ni simbolo'
+    }
+    if(!/^[0-9]+$/i.test(FormData.minHeight)){
+        errors.minHeight = 'solo numeros rey'
+    }
+    if(!/^[0-9]+$/i.test(FormData.maxHeight)){
+        errors.maxHeight = 'solo numeros mi rey'
+    }
+    if(!/^[0-9]+$/i.test(FormData.minWeight)){
+        errors.minWeight = 'solo numeros reina o no aprendiste'
+    }
+    if(!/^[0-9]+$/i.test(FormData.maxWeight)){
+        errors.maxWeight = 'no entendes que es solo numero'
+    }
+    if(!/^[0-9]+$/i.test(FormData.life_span)){
+        errors.life_span = 'escribi solo numeros salameeee'
+    }
+
+    return errors;
+};
+
+export default validacion;
